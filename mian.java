3e3c5a1edd6fc7bff5b45ffe8c0a8d9bc8223598
Muneer Ahmed <85023623+Muneer-113@users.noreply.github.com>
2023-10-09 00:12:09 -0700

    Update mian.java

--- mian.java
+++ mian.java
@@ -2,7 +2,10 @@ class main{
 public static void main(args[]){
 
 
-  System.out.println("Hello world");
+  System.out.println("Hello Innayat");
+    System.out.println("Hello Innayat");
+  
+
 
   System.out.println("Hello World");
  System.out.println("Hello World"); System.out.println("Hello World"); System.out.println("Hello World"); System.out.println("Hello World");
